5a0895b5837a86ba61e985614d2f84c585f5281e
lkraman <laurakraman01@gmail.com>
2019-01-07 11:51:20 -0500

    completed assignment react todo events

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-
 import ToDo from './components/ToDo.js';
 
 class App extends Component {
@@ -11,18 +10,49 @@ class App extends Component {
         { description: 'Walk the cat', isCompleted: true },
         { description: 'Throw the dishes away', isCompleted: false },
         { description: 'Buy new dishes', isCompleted: false }
-      ]
+      ],
+      newTodoDescription: ''
     };
+  }
+
+  handleChange(e) {
+    this.setState({ newTodoDescription: e.target.value })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.newTodoDescription) { return }
+    const newTodo = { description: this.state.newTodoDescription, isCompleted: false };
+    this.setState({ todos: [...this.state.todos, newTodo], newTodoDescription: '' });
+  }
+
+  toggleComplete(index) {
+    const todos = this.state.todos.slice();
+    const todo = todos[index];
+    todo.isCompleted = todo.isCompleted ? false : true;
+    this.setState({ todos: todos });
+  }
+
+  deleteTodo(index) {
+    const arrCopy = this.state.todos.slice();
+    const arrFiltered = arrCopy.filter((todo, i) => i !== index);
+
+    this.setState({ todos: arrFiltered });
   }
 
   render() {
     return (
       <div className="App">
-        <ul>
+       <ul>
           { this.state.todos.map( (todo, index) =>
-          <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
-          )}
+          <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } deleteTodo={ () => this.deleteTodo(index) } />
+        )}
         </ul>
+
+        <form onSubmit={ (e) => this.handleSubmit(e) }>
+          <input type="text" value={this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) } />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
